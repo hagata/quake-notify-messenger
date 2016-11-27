d3b56f32187a3439e446d1c890651841f44d2d66
hagata <genki@hagata.info>
2016-11-27 11:56:53 -0800

    Fixes /webhook route

--- app.js
+++ app.js
@@ -1,37 +1,39 @@
 'use strict'
 
 const Koa = require('koa');
-const route = require('koa-route');
-
-// const parse = require('co-body');
-// // const render = require('./lib/render');
-// const views = require('co-views');
+const router = require('koa-router')();
 const path = require('path');
+
 const app = Koa();
 // const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
 const PORT = process.env.PORT || 3000;
 const VERIFY_TOKEN = 'quake_bot_verify_token';
+
 /**
  * Setup middleware
  */
+router.get('/', function* (next) {
+  this.body = 'Hello, Root'
+});
+router.get('/webhook', hook);
 
-app.use(route.get('/', function* () {
-    this.body = 'Hello'
-}))
-app.use(route.get('/webhook', hook));
+app.use(router.routes())
+  .use(router.allowedMethods())
 
 /**
  * Setup route generators
  */
-function* hook(req, res) {
-    console.log('req hook', req)
+function* hook(hub) {
+  const req = this.request
+
   if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === VERIFY_TOKEN) {
-        console.log("Validating webhook");
-        res.status(200).send(req.query['hub.challenge']);
+    req.query['hub.verify_token'] === VERIFY_TOKEN) {
+    console.log("Validating webhook");
+    this.response.status = 200;
+    this.response.body = req.query['hub.challenge'];
   } else {
     console.error("Failed validation. Make sure the validation tokens match.");
-    res.sendStatus(403);
+    this.response.status = 403;
   }
 };
 
